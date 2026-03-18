14c9018d85eb438d199fa9de79ef89bb8f4363a1
Adarsh Verma <Adarshve23@gmail.com>
2026-03-18 22:54:17 +0530

    Removed git conflict markers from build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 # Exit on error
 set -o errexit
 
@@ -10,9 +9,6 @@ npm run build
 cd ..
 
 echo "--- Building Django Backend ---"
-=======
-set -o errexit
->>>>>>> b2cad1256230f4ba3ff88285a85c376e7d266e18
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
